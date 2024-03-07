478245da338dfbc8fe9704de0cb38b347b6eae8d
hyebln <150016645+hyebln@users.noreply.github.com>
2024-03-07 13:58:25 +0900

    Title: 바이러스 검사, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/바이러스 검사/virus-detector.py
@@ -0,0 +1,16 @@
+n = int(input())
+clients = list(map(int, input().split()))
+check = list(map(int, input().split()))
+
+answer = 0
+for c in clients:
+    num = c - check[0]
+    answer += 1
+    if num <= 0:
+        break
+    while True:
+        num -= check[1]
+        answer +=1
+        if num <= 0:
+            break
+print(answer)
