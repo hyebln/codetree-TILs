d02c94803ab42b878ee9552df6ebc167831456cf
hyebln <150016645+hyebln@users.noreply.github.com>
2024-03-07 15:34:24 +0900

    Title: 정육면체 굴리기, Time: 129ms, Memory: 28MB, Status: Passed - CodeTree

--- 240307/정육면체 굴리기/cube-rounding.py
+++ 240307/정육면체 굴리기/cube-rounding.py
@@ -3,7 +3,7 @@ board = [list(map(int, input().split())) for _ in range(n)]
 directions = list(map(int, input().split()))
 
 dice = [[0, 0], [0, 0], [0, 0]]
-loc = [0, 0]
+loc = [x,y]
 dx = [0, 0, 0, -1, 1] # 동,서,북,남
 dy = [0, 1, -1, 0, 0]
 def move(dir, dice, loc):
@@ -11,16 +11,13 @@ def move(dir, dice, loc):
     nx, ny = cx+dx[dir], cy+dy[dir]
     if 0<=nx<n and 0<=ny<m:
         if dir == 1:
-            dice = [dice[1], dice[0][::-1], dice[2]]
-
+            dice[0], dice[1] = dice[1], dice[0][::-1]
         elif dir == 2:
-            dice = [dice[1][::-1], dice[0], dice[2]]
-
+            dice[0], dice[1] = dice[1][::-1], dice[0]
         elif dir == 3:
-            dice = [dice[2], dice[1], dice[0][::-1]]
-
+            dice[0], dice[2] = dice[2], dice[0][::-1]
         elif dir == 4:
-            dice = [dice[2][::-1], dice[1], dice[0]]
+            dice[0], dice[2] = dice[2][::-1], dice[0]
 
         if board[nx][ny] == 0:
             board[nx][ny] = dice[0][1]
