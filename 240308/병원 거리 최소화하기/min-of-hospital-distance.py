c698cc95e6498ad846a5f50ac9bb81d441fb7654
hyebln <150016645+hyebln@users.noreply.github.com>
2024-03-08 14:51:45 +0900

    Title: 병원 거리 최소화하기, Time: 5043ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240308/병원 거리 최소화하기/min-of-hospital-distance.py
+++ 240308/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -12,29 +12,20 @@ for i in range(n):
 visited = [0]*len(hospital)
 
 
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
 def caldistance(selected):
     global ans
     dist = 0
     for i in range(n):
         for j in range(n):
             if board[i][j] == 1:
-                q = deque()
-                q.append([i,j])
-                visited = [[0]*n for _ in range(n)]
-                visited[i][j] = 1
-                while q:
-                    cx, cy = q.popleft()
-                    if [cx,cy] in selected:
-                        dist += abs(cx-i) + abs(cy-j)
-                        break
-                    for d in range(4):
-                        nx = cx + dx[d]
-                        ny = cy + dy[d]
-                        if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
-                            q.append([nx,ny])
+                minds = 10e9
+                for hp in selected:
+                    ds = abs(i-hp[0]) + abs(j-hp[1])
+                    minds = min(ds, minds)
+                dist += minds
     ans = min(ans, dist)
+
+
 def select(arr, n):
     if n == m:
         caldistance(arr)
