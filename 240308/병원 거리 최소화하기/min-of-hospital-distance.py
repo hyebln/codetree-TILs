7cbf19fd6b381a754a169c2f4232ed1d3e50a4e6
hyebln <150016645+hyebln@users.noreply.github.com>
2024-03-08 15:07:39 +0900

    Title: 병원 거리 최소화하기, Time: 5100ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240308/병원 거리 최소화하기/min-of-hospital-distance.py
+++ 240308/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -1,13 +1,14 @@
-from  collections import deque
-
 n,m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
 hospital = []
+person = []
 for i in range(n):
     for j in range(n):
         if board[i][j] == 2:
             hospital.append([i,j])
+        elif board[i][j] == 1:
+            person.append([i,j])
 
 visited = [0]*len(hospital)
 
@@ -15,22 +16,21 @@ visited = [0]*len(hospital)
 def caldistance(selected):
     global ans
     dist = 0
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] == 1:
-                minds = 10e9
-                for hp in selected:
-                    ds = abs(i-hp[0]) + abs(j-hp[1])
-                    minds = min(ds, minds)
-                dist += minds
+    for i,j in person:
+        minds = 10e9
+        for hp in selected:
+            ds = abs(i-hp[0]) + abs(j-hp[1])
+            minds = min(ds, minds)
+        dist += minds
+
     ans = min(ans, dist)
 
-
 def select(arr, n):
-    if n == m:
+    if len(arr) == m:
         caldistance(arr)
         return
-    for i in range(len(hospital)):
+
+    for i in range(n, len(hospital)):
         if not visited[i]:
             visited[i] = 1
             arr.append(hospital[i])
