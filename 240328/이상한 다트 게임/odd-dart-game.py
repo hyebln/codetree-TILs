c21a0355e1917d4359279c5bf9dcff2afd5ad69b
hyebln <150016645+hyebln@users.noreply.github.com>
2024-03-28 20:56:20 +0900

    Title: 이상한 다트 게임, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240328/이상한 다트 게임/odd-dart-game.py
+++ 240328/이상한 다트 게임/odd-dart-game.py
@@ -14,25 +14,28 @@ def boardRotate(i,d,k):
 def checkBoard():
     dx = [0,0,-1,1]
     dy = [1,-1,0,0]
-    visited = [[0]*n for _ in range(n)]
+    visited = [[0]*m for _ in range(n)]
     deletenum = False
     for i in range(0,n):
-        for j in range(0,n):
+        for j in range(0,m):
+            if board[i][j] ==0:
+                continue
             for d in range(4):
                 ni = (i+dx[d])%n
-                nj = (j+dy[d])%n
+                nj = (j+dy[d])%m
                 if i in [0, n-1]:
                     ni = i + dx[d]
                     if ni<0 or ni>=n:
                         continue
                 if board[ni][nj] == board[i][j]:
+
                     deletenum = True
                     visited[i][j] = 1
                     visited[ni][nj] = 1
 
     if deletenum:
         for i in range(n):
-            for j in range(n):
+            for j in range(m):
                 if visited[i][j]:
                     board[i][j] = 0
     else:
@@ -45,7 +48,7 @@ def checkBoard():
                     sum += board[i][j]
         avg = sum // cnt
         for i in range(n):
-            for j in range(n):
+            for j in range(m):
                 if board[i][j] ==0:
                     continue
                 if board[i][j] > avg:
@@ -54,13 +57,12 @@ def checkBoard():
                     board[i][j] += 1
 
 
-
 def answer():
     ans=0
     for i in range(n):
         ans += sum(board[i])
     print(ans)
-
+    
 for x,d,k in rotate_list:
     boardRotate(x-1,d,k)
     checkBoard()
