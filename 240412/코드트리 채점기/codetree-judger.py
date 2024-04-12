91af4ff5d4d077d47be2082bb21c52af83d06893
hyebln <150016645+hyebln@users.noreply.github.com>
2024-04-12 19:20:41 +0900

    Title: 코드트리 채점기, Time: 817ms, Memory: 36MB, Status: Passed - CodeTree

--- 240412/코드트리 채점기/codetree-judger.py
+++ 240412/코드트리 채점기/codetree-judger.py
@@ -1,86 +1,78 @@
 import heapq
 from collections import defaultdict
-q = int(input())
-queries = [list(input().split()) for _ in range(q)]
 
-def startCalcul(time):
-    newheapq = []
-    bestdom, best_p, best_t = '', 10e9, 10e9
-    while stanby:
-        p,t = heapq.heappop(stanby)
-        domain, id = stanbyurl[t].split('/')
-        if domain in enddomain.keys():
-            start, gap = enddomain[domain]
-            if gap == 0:
-                heapq.heappush(newheapq, [p,t])
-            else:
-                if time < start + 3*gap:
-                    heapq.heappush(newheapq, [p,t])
-                else:
-                    if (best_p, best_t) > (p,t):
-                        if bestdom != '':
-                            heapq.heappush(newheapq, [best_p,best_t])
-                        bestdom, best_p, best_t = domain, p,t
-                    else:
-                        heapq.heappush(newheapq, [p,t])
+
+Q = int(input())
+QUE = [list(input().split()) for _ in range(Q)]
+
+
+waitQ = defaultdict(list)
+waitingurl = set()
+caltime = defaultdict()
+endlist = []
+
+for query in QUE:
+    q, *inp = query
+    if q == '100':
+        N, u0 = inp
+        calcul = [0] * int(N)
+        dom0, id0 = u0.split('/')
+        heapq.heappush(waitQ[dom0], [1, 0, int(id0)])
+        waitingurl.add(u0)
+
+    if q =='200':
+        t,p,u = inp
+        if u in waitingurl:
+            continue
+        dom0, id = u.split('/')
+        heapq.heappush(waitQ[dom0], [int(p), int(t), int(id)])
+        waitingurl.add(u)
+
+    if q == '300':
+        if not 0 in calcul:
             continue
 
-        if (best_p, best_t) > (p, t):
-            if bestdom != '':
-                heapq.heappush(newheapq, [best_p, best_t])
-            bestdom, best_p, best_t = domain, p, t
-        else:
-            heapq.heappush(newheapq, [p,t])
+        t = int(inp[0])
+        bestdom, best_p, best_t = '', 10e9, 10e9
+        for domain, heap in waitQ.items():
+            if domain in caltime.keys():
+                start, gap = caltime[domain]
+                if gap == 0:
+                    continue
+                if gap!=0 and t < start+3*gap:
+                    continue
 
-    if bestdom == '':
-        return newheapq
+            if not heap:
+                continue
 
-    sdomain, sid = stanbyurl.pop(best_t).split('/')
-    for idx, cal in enumerate(calcul):
-        if idx == 0:
+            que = heap[0]
+            qp, qt, qid = que
+            if (qp, qt) < (best_p, best_t):
+                bestdom, best_p, best_t, pid = domain, qp, qt, qid
+
+        if bestdom == '':
             continue
-        if cal == 0:
-            calcul[idx] = [sdomain, time]
-            enddomain[sdomain] = [time, 0]
-            break
 
-    return newheapq
+        heapq.heappop(waitQ[bestdom])
+        besturl = bestdom +'/'+str(pid)
+        for idx, cal in enumerate(calcul):
+            if cal == 0:
+                calcul[idx] = [bestdom, t]
+                caltime[bestdom] = [t,0]
+                waitingurl.remove(besturl)
+                break
 
-def endCalcul(time, jidx):
-    # print(time,'초에 멈춤', jidx)
-    domain, st= calcul[jidx]
-    calcul[jidx] = 0
-    enddomain[domain]=[st, time-st]
-
-stanby = []
-stanbyurl = {}
-enddomain = defaultdict(list)
-for query in queries:
-    qtype, *content = query
-    # print('명령어', query)
-    if qtype=='100':
-        N, u0 = content
-        calcul = [0]*int(int(N)+1)
-        heapq.heappush(stanby, [1,0])
-        stanbyurl.setdefault(0,u0)
-    if qtype == '200':
-        t,p,u = content
-        t, p = int(t), int(p)
-        if u not in stanbyurl.values():
-            heapq.heappush(stanby, [p,t])
-            stanbyurl.setdefault(t, u)
-
-    if qtype == '300':
-        t = int(content[0])
-        if 0 in calcul[1:]:
-            stanby = startCalcul(t)
-
-    if qtype == '400':
-        t, jid = int(content[0]), int(content[1])
-        if calcul[jid] == 0:
+    if q =='400':
+        t, Jid = inp
+        if calcul[int(Jid)-1] == 0:
             continue
-        endCalcul(t, jid)
+        dom,st = calcul[int(Jid)-1]
 
-    if qtype == '500':
-        t = int(content[0])
-        print(len(stanby))
+        calcul[int(Jid)-1] = 0
+        caltime[dom] = [st, int(t)-st]
+
+    if q == '500':
+        ans =0
+        for val in waitQ.values():
+            ans += len(val)
+        print(ans)
