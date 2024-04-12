a8fbbc66a0ab3f5b00714cc48f6526b3748ca727
hyebln <150016645+hyebln@users.noreply.github.com>
2024-04-12 19:18:21 +0900

    Title: 코드트리 채점기, Time: 5068ms, Memory: 44MB, Status: Time Limit Exceed - CodeTree

--- 240412/코드트리 채점기/codetree-judger.py
+++ 240412/코드트리 채점기/codetree-judger.py
@@ -9,15 +9,15 @@ def startCalcul(time):
     while stanby:
         p,t = heapq.heappop(stanby)
         domain, id = stanbyurl[t].split('/')
-        if domain in calcul.values():
-            heapq.heappush(unavailable, [p,t])
-            continue
         if domain in enddomain.keys():
-            curstart, gap = enddomain[domain][-1]
-            if time < curstart + 3*gap:
+            start, gap = enddomain[domain]
+            if gap == 0:
                 heapq.heappush(unavailable, [p,t])
             else:
-                heapq.heappush(available, [p,t])
+                if time < start + 3*gap:
+                    heapq.heappush(unavailable, [p,t])
+                else:
+                    heapq.heappush(available, [p,t])
             continue
         heapq.heappush(available, [p,t])
 
@@ -30,33 +30,29 @@ def startCalcul(time):
 
     sp, st = selected
     sdomain, sid = stanbyurl.pop(st).split('/')
-    for idx, cur in calcul.items():
-        if not cur:
-            calcul[idx] = sdomain
-            startT[idx] = time
+    for idx, cal in enumerate(calcul):
+        if idx == 0:
+            continue
+        if cal == 0:
+            calcul[idx] = [sdomain, time]
+            enddomain[sdomain] = [time, 0]
             break
 
     return unavailable
 
 def endCalcul(time, jidx):
-    domain = calcul[jidx]
-    st = startT[jidx]
-    calcul[jidx] = []
-    startT[jidx] = 0
-    enddomain[domain].append([st, time-st])
+    domain, st= calcul[jidx]
+    calcul[jidx] = 0
+    enddomain[domain]=[st, time-st]
 
 stanby = []
 stanbyurl = {}
 enddomain = defaultdict(list)
-calcul = {}
-startT = [0]
 for query in queries:
     qtype, *content = query
     if qtype=='100':
         N, u0 = content
-        for i in range(1, int(N)+1):
-            calcul.setdefault(i, [])
-            startT.append(0)
+        calcul = [0]*int(int(N)+1)
         heapq.heappush(stanby, [1,0])
         stanbyurl.setdefault(0,u0)
     if qtype == '200':
@@ -67,12 +63,12 @@ for query in queries:
             stanbyurl.setdefault(t, u)
     if qtype == '300':
         t = int(content[0])
-        if list(calcul.values()).count([]) == 0:
+        if not 0 in calcul[1:]:
             continue
         stanby = startCalcul(t)
     if qtype == '400':
         t, jid = int(content[0]), int(content[1])
-        if calcul[jid] == []:
+        if calcul[jid] == 0:
             continue
         endCalcul(t, jid)
 
