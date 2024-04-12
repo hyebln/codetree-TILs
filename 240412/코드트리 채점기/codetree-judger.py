a9dd679c54449aab88a3527da016f262f69112fd
hyebln <150016645+hyebln@users.noreply.github.com>
2024-04-12 19:18:55 +0900

    Title: 코드트리 채점기, Time: 5068ms, Memory: 44MB, Status: Time Limit Exceed - CodeTree

--- 240412/코드트리 채점기/codetree-judger.py
+++ 240412/코드트리 채점기/codetree-judger.py
@@ -4,32 +4,38 @@ q = int(input())
 queries = [list(input().split()) for _ in range(q)]
 
 def startCalcul(time):
-    available = []
-    unavailable = []
+    newheapq = []
+    bestdom, best_p, best_t = '', 10e9, 10e9
     while stanby:
         p,t = heapq.heappop(stanby)
         domain, id = stanbyurl[t].split('/')
         if domain in enddomain.keys():
             start, gap = enddomain[domain]
             if gap == 0:
-                heapq.heappush(unavailable, [p,t])
+                heapq.heappush(newheapq, [p,t])
             else:
                 if time < start + 3*gap:
-                    heapq.heappush(unavailable, [p,t])
+                    heapq.heappush(newheapq, [p,t])
                 else:
-                    heapq.heappush(available, [p,t])
+                    if (best_p, best_t) > (p,t):
+                        if bestdom != '':
+                            heapq.heappush(newheapq, [best_p,best_t])
+                        bestdom, best_p, best_t = domain, p,t
+                    else:
+                        heapq.heappush(newheapq, [p,t])
             continue
-        heapq.heappush(available, [p,t])
 
-    if available == []:
-        return unavailable
+        if (best_p, best_t) > (p, t):
+            if bestdom != '':
+                heapq.heappush(newheapq, [best_p, best_t])
+            bestdom, best_p, best_t = domain, p, t
+        else:
+            heapq.heappush(newheapq, [p,t])
 
-    selected = heapq.heappop(available)
-    for remain in available:
-        heapq.heappush(unavailable, remain)
+    if bestdom == '':
+        return newheapq
 
-    sp, st = selected
-    sdomain, sid = stanbyurl.pop(st).split('/')
+    sdomain, sid = stanbyurl.pop(best_t).split('/')
     for idx, cal in enumerate(calcul):
         if idx == 0:
             continue
@@ -38,9 +44,10 @@ def startCalcul(time):
             enddomain[sdomain] = [time, 0]
             break
 
-    return unavailable
+    return newheapq
 
 def endCalcul(time, jidx):
+    # print(time,'초에 멈춤', jidx)
     domain, st= calcul[jidx]
     calcul[jidx] = 0
     enddomain[domain]=[st, time-st]
@@ -50,6 +57,7 @@ stanbyurl = {}
 enddomain = defaultdict(list)
 for query in queries:
     qtype, *content = query
+    # print('명령어', query)
     if qtype=='100':
         N, u0 = content
         calcul = [0]*int(int(N)+1)
@@ -61,11 +69,12 @@ for query in queries:
         if u not in stanbyurl.values():
             heapq.heappush(stanby, [p,t])
             stanbyurl.setdefault(t, u)
+
     if qtype == '300':
         t = int(content[0])
-        if not 0 in calcul[1:]:
-            continue
-        stanby = startCalcul(t)
+        if 0 in calcul[1:]:
+            stanby = startCalcul(t)
+
     if qtype == '400':
         t, jid = int(content[0]), int(content[1])
         if calcul[jid] == 0:
